b30e028ead21c1957b75af15c52f86567b387b21
Jonathan Harford <jonathan.harford@gmail.com>
2014-03-12 17:53:06 -0400

    Removed some defunct testing code in prep for separating out merging code

--- Results_Download.py
+++ Results_Download.py
@@ -9,16 +9,14 @@ import pandas as pd
 from selenium import webdriver
 
 from excelEnnumerations import *
+#from append_xls import *
 from logininfo import *
 
 def series_to_iso8601(ser):
     """Return a series of ISO 8601 dates from a series of datetimes."""
 
     def to_iso8601(x):
-        if x > pd.datetime(1900,1,1):
-            return x.strftime('%Y-%m-%d')
-        else:
-            return ''
+        return x.strftime('%Y-%m-%d') if (x > pd.datetime(1900,1,1)) else ''
 
     return ser.apply(to_iso8601)
 
@@ -105,34 +103,26 @@ def download_effort_results(nav, efforts_to_download):
     return df
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        filename = "testdata"
-        print __doc__
-        actually_download = False
-    else: actually_download = True
 
-    if actually_download:
-        filename = sys.argv[1]
-        # Get list of which efforts we want results for
-        packages_to_download = [line.strip().split("\t") for line in open(filename, 'rb').readlines()]
+    filename = sys.argv[1]
+    # Get list of which efforts we want results for
+    packages_to_download = [line.strip().split("\t") for line in open(filename, 'rb').readlines()]
 
-        us_pkgs_download = [pkg for org, pkg in packages_to_download if org==US_NAV['org']]
-        ny_pkgs_download = [pkg for org, pkg in packages_to_download if org==NY_NAV['org']]
+    us_pkgs_download = [pkg for org, pkg in packages_to_download if org==US_NAV['org']]
+    ny_pkgs_download = [pkg for org, pkg in packages_to_download if org==NY_NAV['org']]
 
-        # Download the results for US and NY.
-        # We could do the downloads simultaneously (threads), but that causes its own troubles.
+    # Download the results for US and NY.
+    # We could do the downloads simultaneously (threads), but that causes its own troubles.
 
-    df =           download_effort_results(US_NAV, us_pkgs_download)
+    df_us = download_effort_results(US_NAV, us_pkgs_download)
+    df_ny = download_effort_results(NY_NAV, ny_pkgs_download)
 
     # Remember the right order of columns, because 'append' alphasorts:
-    col_order = df.columns
+    col_order = df_us.columns
 
-    df = df.append(download_effort_results(NY_NAV, ny_pkgs_download))
+    df = df_us.append(df_ny)
 
     df = df[col_order] # Fix the order of cols.
-
-    # df = df.set_index("Mail Code", verify_integrity=True)
-    # df = df[['Mail Code','DM Donors','DM Revenue','Descript','FF Date','Mail Date','ND Count','Package','Qty Mail','TM Donors','TM Revenue','Total Donors','Total Revenue','Web Donors','Web Revenue']]
 
     df['Mail Date']  = series_to_iso8601(df['Mail Date'])
     df['FF Date']    = series_to_iso8601(df['FF Date'])
@@ -140,7 +130,7 @@ if __name__ == "__main__":
     df['Pack Date'] = series_to_iso8601(df['Pack Date'])
 
     df.to_csv(os.path.splitext(filename)[0] + '.csv', encoding='utf-8', index_label='Mail Code')
-    # XLSX is VERY slow compared to csv:
-    #df.to_excel(os.path.splitext(filename)[0] + ".xlsx")
+    # We could export to XLSX instead but it's much slower:
+    # df.to_excel(os.path.splitext(filename)[0] + ".xlsx")
 
 
